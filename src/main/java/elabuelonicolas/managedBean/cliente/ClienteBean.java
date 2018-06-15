b7f71dba4e50a0e975fe4538ce9c9a30c142bd97
IrvingMg <mirvingr@gmail.com>
2018-06-15 18:27:08 -0500

    Insertar cliente y tema primefaces

--- src/main/java/elabuelonicolas/managedBean/cliente/ClienteBean.java
+++ src/main/java/elabuelonicolas/managedBean/cliente/ClienteBean.java
@@ -1,5 +1,7 @@
 package elabuelonicolas.managedBean.cliente;
 
+
+import javax.faces.bean.ManagedBean;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -15,6 +17,19 @@ public class ClienteBean {
 	@Inject
 	ClienteService clienteService;
 	private List<Cliente> clienteList;
+	//Campos cliente
+	private String nombre;
+    private String contacto;
+    private String telefono;
+    private String email;
+    private String rfc;
+    //Campos direccion
+    private String pais;
+    private String estado;
+    private String ciudad;
+    private String colonia;
+    private String calle;
+    private String numero;
 
 	public List<Cliente> getClienteList() {
 		if (clienteList == null)
@@ -31,7 +46,7 @@ public class ClienteBean {
 		Cliente cliente = ((Cliente) event.getObject());
 		System.out.println("Datos cliente: " + cliente.getId());
 		clienteService.update(cliente);
-
+	
 		FacesMessage msg = new FacesMessage("Cliente editado", cliente.getId()
 				.toString());
 		FacesContext.getCurrentInstance().addMessage(null, msg);
@@ -60,4 +75,132 @@ public class ClienteBean {
 		clienteService.delete(order.getId());
 		return null;
 	}
+	    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+ 
+    public String getContacto() {
+        return contacto;
+    }
+    
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+    
+    public String getTelefono() {
+        return telefono;
+    }
+    
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getRfc() {
+        return rfc;
+    }
+    
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+    
+    public String getPais() {
+        return pais;
+    }
+    
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
+    public String getEstado() {
+        return estado;
+    }
+    
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+ 
+    public String getCiudad() {
+        return ciudad;
+    }
+    
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+    
+    public String getColonia() {
+        return colonia;
+    }
+    
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+    
+    public String getCalle() {
+        return calle;
+    }
+    
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+    
+    public String getNumero() {
+        return numero;
+    }
+    
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+    
+    public void save() {
+        
+    	//Insertar dirección
+    	
+    	//Insertar cliente
+        Cliente nuevoCliente = new Cliente();
+        nuevoCliente.setNombre(nombre);
+        nuevoCliente.setIddireccion(20); //Obtener id de la ultima dirección insertada ala tabla
+        nuevoCliente.setContacto(contacto);
+        nuevoCliente.setTelefono(telefono);
+        nuevoCliente.setEmail(email);
+        nuevoCliente.setRfc(rfc);
+        
+        clienteService.create(nuevoCliente);
+        nuevoCliente.setId(clienteService.last().getId()); 
+        clienteList.add(nuevoCliente);
+       
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Cliente " + nombre + " registrado "));
+        
+        resetFormulario();
+       
+    }
+    
+    @SuppressWarnings("null")
+	public void resetFormulario() {
+    	this.nombre = null;
+        this.contacto = null;
+        this.telefono = null;
+        this.email = null;
+        this.rfc = null;
+        this.pais = null;
+        this.estado = null;
+        this.ciudad = null;
+        this.colonia = null;
+        this.calle = null;
+        this.numero = null;
+    }
 }
